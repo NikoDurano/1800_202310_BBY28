6063d48b0e6b0adda22f1ffb627048a530552fdb
Niko Durano <nikodurano@gmail.com>
2023-03-01 18:05:34 -0800

    Fixed pathing for files

--- public/js/skeleton.js
+++ public/js/skeleton.js
@@ -3,7 +3,7 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./standard/topNav.html'));
-    console.log($('#footerPlaceholder').load('./standard/botNav.html'));
+    console.log($('#navbarPlaceholder').load('/app/standard/topNav.html'));
+    console.log($('#footerPlaceholder').load('/app/standard/botNav.html'));
 }
 loadSkeleton();  //invoke the function
