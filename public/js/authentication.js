0cb190462d543f4fb035c5b3562625307365456e
Niko Durano <nikodurano@gmail.com>
2023-03-04 14:24:32 -0800

    Checkpoint before Read/write data

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -41,7 +41,6 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    //signInSuccessUrl: 'tabSymptomTracker.html',
     signInSuccessUrl: 'tabSymptomTracker.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
