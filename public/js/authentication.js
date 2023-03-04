4b67919fb78838b58c9be6b39f136b7816b66cd5
Niko Durano <nikodurano@gmail.com>
2023-03-04 13:57:05 -0800

    New Users go to profile creator page FireStore set up

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -3,12 +3,36 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
     callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
         // User successfully signed in.
         // Return type determines whether we continue the redirect automatically
         // or whether we leave that to developer to handle.
-        return true;
-      },
+        //------------------------------------------------------------------------------------------
+        // The code below is modified from default snippet provided by the FB documentation.
+        //
+        // If the user is a "brand new" user, then create a new "user" in your own database.
+        // Assign this user with the name and email provided.
+        // Before this works, you must enable "Firestore" from the firebase console.
+        // The Firestore rules must allow the user to write. 
+        //------------------------------------------------------------------------------------------
+        var user = authResult.user;                            // get the user object from the Firebase authentication database
+        if (authResult.additionalUserInfo.isNewUser) {         //if new user
+            db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
+                   name: user.displayName,                    //"users" collection
+                   email: user.email,                         //with authenticated user's ID (user.uid)
+                   country: "Canada",                      //optional default profile info      
+                   school: "BCIT"                          //optional default profile info
+            }).then(function () {
+                   console.log("New user added to firestore");
+                   window.location.assign("petProfileCreator.html");       //re-direct to main.html after signup
+            }).catch(function (error) {
+                   console.log("Error adding new user: " + error);
+            });
+        } else {
+            return true;
+        }
+            return false;
+        },
       uiShown: function() {
         // The widget is rendered.
         // Hide the loader.
@@ -17,6 +41,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
+    //signInSuccessUrl: 'tabSymptomTracker.html',
     signInSuccessUrl: 'tabSymptomTracker.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,44 @@
-function sayHello() {
-    
+// Function to read the quote of the day from Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(Hello) {
+
+         //CHANGE "test" TO COLLECTION NAME
+    db.collection("test").doc(Hello)  
+                                                        //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(tuesdayDoc => {                                                               //arrow notation
+           console.log("current document data: " + tuesdayDoc.data());                          //.data() returns data object
+                          //CHANGE "quote-goes-here" TO ID OF SPAN          CHANGE "Hello" TO KEY VALUE PAIR
+           document.getElementById("quote-goes-here").innerHTML = tuesdayDoc.data().Hello;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(tuesdayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = tuesdayDoc.data().quote;
+      })
 }
-//sayHello();
+ //CHANGE "thisOne" TO DOCUMENT NAME
+readQuote("thisOne");        //calling the function
+
+
+
+
+
+
+/* // Function to read the quote of the day from Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(day) {
+
+
+    db.collection("quotes").doc(day)  
+                                                        //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(tuesdayDoc => {                                                               //arrow notation
+           console.log("current document data: " + tuesdayDoc.data());                          //.data() returns data object
+           document.getElementById("quote-goes-here").innerHTML = tuesdayDoc.data().quote;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(tuesdayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = tuesdayDoc.data().quote;
+      })
+}
+readQuote("tuesday");        //calling the function */
