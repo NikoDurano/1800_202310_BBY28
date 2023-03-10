d7bc5e88ea1a9354257db4099ce679ab876d0855
Niko Durano <nikodurano@gmail.com>
2023-03-09 21:52:14 -0800

    Pet profile overhaul

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -84,7 +84,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'tabSymptomTracker.html',
+    signInSuccessUrl: 'tabPetProfile.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
 /*       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-// Function to read the quote of the day from Firestore "quotes" collection
+/* // Function to read the quote of the day from Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
 function readQuote(Hello) {
 
@@ -17,7 +17,7 @@ function readQuote(Hello) {
       })
 }
  //CHANGE "thisOne" TO DOCUMENT NAME
-readQuote("thisOne");        //calling the function
+readQuote("thisOne");        //calling the function */
 
 
 function insertNameFromFirestore(){
@@ -26,21 +26,86 @@ function insertNameFromFirestore(){
          if (user){
             console.log(user.uid); // let me to know who is the user that logged in to get the UID
             currentUser = db.collection("users").doc(user.uid).collection("petInfo").doc(user.uid); // will to to the firestore and go to the document of the user
+            
             currentUser.get().then(userDoc=>{
                 //get the user name
                 var userName= userDoc.data().namePet;
                 var userAge = userDoc.data().agePet;
+                var userWeight = userDoc.data().weightPet;
                 console.log(userName);
                 console.log(userAge);
                 //$("#name-goes-here").text(userName); //jquery
-                document.getElementById("petInfo").innerText=userName +", "+ userAge;
+                document.getElementById("petNameBold").innerText=userName;
+                document.getElementById("petAge").innerText=userAge;
+                document.getElementById("petWeight").innerText=userWeight;
+
  
 
-            })    
+            })  
         }    
      })
 }
 insertNameFromFirestore();
+
+
+
+
+
+function insertVetFromFirestore(){
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user =>{
+        if (user){
+           console.log(user.uid); // let me to know who is the user that logged in to get the UID
+           currentuserVet = db.collection("users").doc(user.uid).collection("petInfo").doc(user.uid).collection("vetInfo").doc(user.uid);
+            
+           currentuserVet.get().then(userDoc=>{
+               //get the user name
+               var vetName= userDoc.data().nameVet;
+               var vetAddress = userDoc.data().addressVet;
+               var vetPhone = userDoc.data().phoneNumberVet;
+               //$("#name-goes-here").text(userName); //jquery
+               document.getElementById("vetNAME").innerText=vetName;
+               document.getElementById("vetADDRESS").innerText=vetAddress;
+               document.getElementById("vetNUMBER").innerText=vetPhone;
+           }) 
+       }    
+    })
+}
+insertVetFromFirestore();
+
+function insertConditionFromFirestore(){
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user =>{
+        if (user){
+           console.log(user.uid); // let me to know who is the user that logged in to get the UID
+           currentuser = db.collection("users").doc(user.uid).collection("petInfo").doc(user.uid);
+            
+           currentuser.get().then(userDoc=>{
+               //get the user name
+               var coat= userDoc.data().coatType;
+               var face = userDoc.data().flatFace;
+               var brain = userDoc.data().brainProblem;
+               var heart = userDoc.data().heartProblem;
+               var lungs = userDoc.data().lungProblem;
+               //$("#name-goes-here").text(userName); //jquery
+               document.getElementById("coat").innerText="Coat Type: "+ coat;
+               document.getElementById("face").innerText="Flat Face Breed: "+face;
+               document.getElementById("brain").innerText="Existing Neurological Issues: "+brain;
+               document.getElementById("heart").innerText="Existing Heart Issues: "+heart;
+               document.getElementById("lung").innerText="Existing Respiratory Issues: "+lungs;
+
+           }) 
+       }    
+    })
+}
+insertConditionFromFirestore();
+
+
+
+
+
+
+
 
 
 
