26213ecbd1a9a257679bdbe9a0987d276657133e
Justin Saint <justinsaintdev@gmail.com>
2023-03-31 09:01:16 -0700

    Minor updates

--- public/js/petTips.js
+++ public/js/petTips.js
@@ -1,15 +1,13 @@
-// hide all content divs except content1 when the page loads
+
 window.addEventListener('load', function() {
-    // get all content divs
+
     const contentDivs = document.querySelectorAll('.content');
     
-    // loop through each content div
+
     for (let i = 0; i < contentDivs.length; i++) {
-      // if it's the first content div, display it
       if (i === 0) {
         contentDivs[i].style.display = 'block';
       } 
-      // otherwise, hide it
       else {
         contentDivs[i].style.display = 'none';
       }
@@ -17,16 +15,12 @@ window.addEventListener('load', function() {
   });
   
   function showContent(contentId) {
-    // get all content divs
     const contentDivs = document.querySelectorAll('.content');
     
-    // loop through each content div
     for (let i = 0; i < contentDivs.length; i++) {
-      // if it's the content div we want to show, display it
       if (contentDivs[i].id === contentId) {
         contentDivs[i].style.display = 'block';
       } 
-      // otherwise, hide it
       else {
         contentDivs[i].style.display = 'none';
       }
--- public/js/symptom4Physical.js
+++ public/js/symptom4Physical.js
@@ -287,12 +287,9 @@ function petSymptoms(behavior, physical) {
 
 function writeTrackerLog() {
     console.log("Tracker Entries")
-    let Title = document.getElementById("title").value;
+    let temperature = document.getElementById("title").value;
     let Level = document.getElementById("level").value;
     let Season = document.getElementById("season").value;
-    let Description = document.getElementById("description").value;
-    let Flooded = document.querySelector('input[name="flooded"]:checked').value;
-    let Scrambled = document.querySelector('input[name="scrambled"]:checked').value;
     console.log(Title, Level, Season, Description, Flooded, Scrambled);
 
     firebase.auth().onAuthStateChanged(user => {
