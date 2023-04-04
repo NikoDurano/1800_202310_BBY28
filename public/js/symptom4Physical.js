cce5ea22090c1f5049a6a64e0b695df20f285692
Justin Saint <justinsaintdev@gmail.com>
2023-04-04 11:31:57 -0700

    Updating for merge

--- public/js/symptom4Physical.js
+++ public/js/symptom4Physical.js
@@ -21,7 +21,7 @@ function physical() {
       arrPhysical: arrPhysicals,
     })
     .then(() => {
-   /*    alert("works"); */
+      /*    alert("works"); */
       getValues();
     })
     .catch((error) => {
@@ -161,7 +161,7 @@ function lowRisk() {
 
   close1.addEventListener("click", () => {
     modelCon1.classList.remove("show");
-    window.location.href = "home.html"; 
+    window.location.href = "home.html";
 
   });
   modelCon1.classList.add("show");
@@ -206,18 +206,20 @@ function petWeight(temperature, coat, size) {
 
   if (size == "Obese") {
     sizeValue += 1;
+
     if (temperature >= 19) {
       sizeValue += 3;
 
       if (coat == "long") {
         sizeValue += 3;
+
       }
     }
   } else if (
-    (size == "Thin" && temperature <= 11) ||
-    (size == "Overweight" && temperature >= 19) ||
-    (coat == "short" && temperature <= 11) ||
-    (coat == "long" && temperature >= 19)
+    (size == "Thin" && temperature <= 0) ||
+    (size == "Overweight" && temperature >= 30) ||
+    (coat == "short" && temperature <= 0) ||
+    (coat == "long" && temperature >= 30)
   ) {
     sizeValue = 3;
   } else {
@@ -290,37 +292,40 @@ function petSymptoms(behavior, physical) {
 }
 
 function writeTrackerLog() {
-    console.log("Tracker Entries")
-    let temperature = document.getElementById("title").value;
-    let Level = document.getElementById("level").value;
-    let Season = document.getElementById("season").value;
-    console.log(Title, Level, Season, Description, Flooded, Scrambled);
+  console.log("Tracker Entries")
+  let Title = document.getElementById("title").value;
+  let Level = document.getElementById("level").value;
+  let Season = document.getElementById("season").value;
+  let Description = document.getElementById("description").value;
+  let Flooded = document.querySelector('input[name="flooded"]:checked').value;
+  let Scrambled = document.querySelector('input[name="scrambled"]:checked').value;
+  console.log(Title, Level, Season, Description, Flooded, Scrambled);
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            var currentUser = db.collection("users").doc(user.uid)
-            var userID = user.uid;
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    var userEmail = userDoc.data().email;
-                    db.collection("reviews").add({
-                        hikeDocID: hikeDocID,
-                        userID: userID,
-                        title: Title,
-                        level: Level,
-                        season: Season,
-                        description: Description,
-                        flooded: Flooded,
-                        scrambled: Scrambled,
-                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                    }).then(() => {
-                        window.location.href = "thanks.html"; //new line added
-                    })
-                })
-        } else {
-            console.log("No user is signed in");
-            window.location.href = 'review.html';
-        }
-    });
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid)
+      var userID = user.uid;
+      //get the document for current user.
+      currentUser.get()
+        .then(userDoc => {
+          var userEmail = userDoc.data().email;
+          db.collection("reviews").add({
+            hikeDocID: hikeDocID,
+            userID: userID,
+            title: Title,
+            level: Level,
+            season: Season,
+            description: Description,
+            flooded: Flooded,
+            scrambled: Scrambled,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+          }).then(() => {
+            window.location.href = "thanks.html"; //new line added
+          })
+        })
+    } else {
+      console.log("No user is signed in");
+      window.location.href = 'review.html';
+    }
+  });
 }
